1e9b42736fb8356c53ae4fbb7a8a34ba8f8b66a2
Chiru <chirudevops3@gmail.com>
2020-07-19 10:50:12 +0000

    Helloword by chiru-1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world by chiru");
-    System.out.println("Hello world by chiru");
-    System.out.println("Hello world by chiru");
+    System.out.println("Hello  chiru");
+    System.out.println("Hello");
     System.out.println("Hello YOU");
     System.out.println("Hello ALL");
   }
